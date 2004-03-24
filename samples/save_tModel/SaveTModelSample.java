3daeca54e2efa41012e77c2a9e32e792a126fdb8
Stephen Viens <sviens@apache.org>
2004-03-24 03:01:55 +0000

    Updated and tested successfully
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/trunk@262716 13f79535-47bb-0310-9956-ffa450edef68

--- samples/save_tModel/SaveTModelSample.java
+++ samples/save_tModel/SaveTModelSample.java
@@ -1,5 +1,16 @@
+import java.util.Properties;
+import java.util.Vector;
+
+import org.apache.juddi.datatype.Description;
+import org.apache.juddi.datatype.KeyedReference;
+import org.apache.juddi.datatype.OverviewDoc;
+import org.apache.juddi.datatype.request.AuthInfo;
+import org.apache.juddi.datatype.response.AuthToken;
+import org.apache.juddi.datatype.response.TModelDetail;
+import org.apache.juddi.datatype.tmodel.TModel;
 import org.apache.juddi.proxy.RegistryProxy;
 import org.apache.juddi.registry.IRegistry;
+import org.apache.juddi.util.Language;
 
 /*
  * Copyright 2001-2004 The Apache Software Foundation.
@@ -25,10 +36,53 @@ public class SaveTModelSample
 {
   public static void main(String[] args)
   {
-    IRegistry registry = new RegistryProxy();
+    // Option #1 (grabs properties from juddi.properties)
+    //IRegistry registry = new RegistryProxy();
+    
+    // Option #2
+    Properties props = new Properties();
+    props.setProperty(RegistryProxy.ADMIN_ENDPOINT_PROPERTY_NAME,"http://localhost:8080/juddi/admin");
+    props.setProperty(RegistryProxy.INQUIRY_ENDPOINT_PROPERTY_NAME,"http://localhost:8080/juddi/inquiry");
+    props.setProperty(RegistryProxy.PUBLISH_ENDPOINT_PROPERTY_NAME,"http://localhost:8080/juddi/publish");
+    IRegistry registry = new RegistryProxy(props);
+
+    String userID = "sviens";
+    String password = "password";
 
     try
-    {
+    {      
+      System.out.println("discard_authToken Sample");
+      System.out.println("------------------------");
+      System.out.println("userID: "+userID);
+      System.out.println("password: "+password);
+      System.out.println("");
+
+      AuthToken authToken = registry.getAuthToken(userID,password);
+      AuthInfo authInfo = authToken.getAuthInfo();
+      System.out.println("AuthToken: "+authInfo.getValue());
+
+      OverviewDoc overviewDoc = new OverviewDoc();
+      overviewDoc.setOverviewURL("http://www.viens.net/uddi/taxonomy_models.html#domain");
+      overviewDoc.addDescription(new Description("Taxonomy used to categorize entities by top level domain.",Language.ENGLISH));
+            
+      TModel tModelIn = new TModel();
+      tModelIn.setAuthorizedName("Steve Viens");
+      tModelIn.setOperator("Viens.net");
+      tModelIn.setName("viens-net:domain");
+      tModelIn.setOverviewDoc(overviewDoc);
+      tModelIn.addDescription(new Description("Categorize entities by top level domain name.",Language.ENGLISH));
+      tModelIn.addCategory(new KeyedReference("uuid:C1ACF26D-9672-4404-9D70-39B756E62AB4","types","categorization"));
+      tModelIn.addCategory(new KeyedReference("uuid:C1ACF26D-9672-4404-9D70-39B756E62AB4","types","checked"));
+      
+      Vector tModelVector = new Vector();
+      tModelVector.add(tModelIn);
+      
+      TModelDetail detail = registry.saveTModel(authInfo,tModelVector);
+      TModel tModelOut = null;
+      if (detail.getTModelVector().size() > 0)
+        tModelOut = (TModel)detail.getTModelVector().elementAt(0);
+        
+      System.out.println("TModel Key: "+tModelOut.getTModelKey());
     }
     catch(Exception ex)
     {
