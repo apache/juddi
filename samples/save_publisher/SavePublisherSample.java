0a15eccd38a74d3b8b7334a2ee1888b002f24c82
Stephen Viens <sviens@apache.org>
2004-04-04 04:17:40 +0000

    Updated and tested successfully.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/trunk@262733 13f79535-47bb-0310-9956-ffa450edef68

--- samples/save_publisher/SavePublisherSample.java
+++ samples/save_publisher/SavePublisherSample.java
@@ -14,12 +14,17 @@
  * limitations under the License.
  */
 
+import java.util.Properties;
 import java.util.Vector;
 
 import org.apache.juddi.datatype.publisher.Publisher;
 import org.apache.juddi.datatype.request.AuthInfo;
 import org.apache.juddi.datatype.response.AuthToken;
+import org.apache.juddi.datatype.response.DispositionReport;
+import org.apache.juddi.datatype.response.ErrInfo;
 import org.apache.juddi.datatype.response.PublisherDetail;
+import org.apache.juddi.datatype.response.Result;
+import org.apache.juddi.error.RegistryException;
 import org.apache.juddi.proxy.RegistryProxy;
 import org.apache.juddi.registry.IRegistry;
 
@@ -31,29 +36,62 @@ public class SavePublisherSample
 {
   public static void main(String[] args)
   {
-    IRegistry registry = new RegistryProxy();
+    // Option #1 (grabs properties from juddi.properties)
+    //IRegistry registry = new RegistryProxy();
+    
+    // Option #2
+    Properties props = new Properties();
+    props.setProperty(RegistryProxy.ADMIN_ENDPOINT_PROPERTY_NAME,"http://localhost:8080/juddi/admin");
+    props.setProperty(RegistryProxy.INQUIRY_ENDPOINT_PROPERTY_NAME,"http://localhost:8080/juddi/inquiry");
+    props.setProperty(RegistryProxy.PUBLISH_ENDPOINT_PROPERTY_NAME,"http://localhost:8080/juddi/publish");
+    IRegistry registry = new RegistryProxy(props);
+
+    String userID = "sviens";
+    String password = "password";
 
     try
     {
-      // execute a GetAuthToken request
-      AuthToken token = registry.getAuthToken("sviens", "password");
-      AuthInfo authInfo = token.getAuthInfo();
+      System.out.println("save_tModel Sample");
+      System.out.println("------------------");
+      System.out.println("userID: "+userID);
+      System.out.println("password: "+password);
+      System.out.println("");
+
+      AuthToken authToken = registry.getAuthToken(userID,password);
+      AuthInfo authInfo = authToken.getAuthInfo();
+      System.out.println("AuthToken: "+authInfo.getValue());
 
       // create a publisher
-      Publisher publisher = new Publisher("BlueNoteIdentifier", "Blue Note");
+      Publisher publisher = new Publisher("acutright","Andy Cutright",false);
 
       // put the Publisher object into a Vector
       Vector vector = new Vector(1);
       vector.add(publisher);
 
       // make the request
-      PublisherDetail detail = registry.savePublisher(authInfo, vector);
+      PublisherDetail detail = registry.savePublisher(authInfo,vector);
 
       System.out.println("publishers saved = " + detail.getPublisherVector().size());
-      }
-    catch(Exception ex)
-    {
-      ex.printStackTrace();
     }
+    catch (RegistryException regex)
+    {
+      DispositionReport dispReport = regex.getDispositionReport();
+      Vector resultVector = dispReport.getResultVector();
+      if (resultVector != null)
+      {
+        Result result = (Result)resultVector.elementAt(0);
+        int errNo = result.getErrno();
+        System.out.println("Errno:   "+errNo);
+
+        ErrInfo errInfo = result.getErrInfo();
+        if (errInfo != null)
+        {
+          System.out.println("ErrCode: "+errInfo.getErrCode());
+          System.out.println("ErrMsg:  "+errInfo.getErrMsg());
+        }        
+      }
+    }
+
+    System.out.println("\ndone.");    
   }
 }
