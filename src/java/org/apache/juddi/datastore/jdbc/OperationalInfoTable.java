d5eb460b5c66757e65f409c0263a6ba5531b8122
Andrew Scott Cutright <acutright@apache.org>
2004-04-08 21:47:42 +0000

    initial subscription API work
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/trunk@262755 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/juddi/datastore/jdbc/OperationalInfoTable.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.juddi.datastore.jdbc;
+
+// java imports
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Vector;
+
+// apache imports
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author Andy Cutright (acutright@apache.org)
+ */
+
+class OperationalInfoTable {
+  private static Log log = LogFactory.getLog(OperationalInfoTable.class);
+  private static String insertSQL = null;
+  static {
+    StringBuffer sql = null;
+    sql = new StringBuffer(256); // @todo right size?
+    sql.append("INSERT INTO OPERATIONAL_INFO ");
+    sql.append("(CREATED_DATE,");
+    sql.append("CREATED_TIME,");
+    sql.append("MODIFIED_DATE,");
+    sql.append("MODIFIED_TIME,");
+    sql.append("MODIFIED_INCLUDING_CHILDREN_DATE,");
+    sql.append("MODIFIED_INCLUDING_CHILDREN_TIME,");
+    sql.append("ENTITY_KEY)");
+    sql.append(" VALUES ");
+    sql.append("(?,?,?,?,?,?,?)");
+    insertSQL = sql.toString();
+  }
+
+  /***************************************************************************/
+  /***************************** TEST DRIVER *********************************/
+  /***************************************************************************/
+  public static void main(String [] args) {
+    OperationalInfoTable.test();
+  }
+  public static void test() {
+    System.out.println("insertSQL: [" + insertSQL + "]");
+  }
+}
