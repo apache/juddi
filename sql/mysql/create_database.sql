df45e5f8b5277d3eabeae8f640f855867044e4e1
Anil Saldhana <anil@apache.org>
2004-05-28 14:59:36 +0000

    Small change:  rename "JUDDI" to "juddi"  as per Steve
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/trunk@262801 13f79535-47bb-0310-9956-ffa450edef68

--- sql/mysql/create_database.sql
+++ sql/mysql/create_database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE JUDDI;
+CREATE DATABASE juddi;
 
-GRANT ALL ON JUDDI.* TO juddi@"%" IDENTIFIED BY "password";
-GRANT ALL ON JUDDI.* TO juddi@"localhost" IDENTIFIED BY "password";
+GRANT ALL ON juddi.* TO juddi@"%" IDENTIFIED BY "password";
+GRANT ALL ON juddi.* TO juddi@"localhost" IDENTIFIED BY "password";
