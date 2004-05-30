0a436c4f522860e9fc19a77c54278e95b035b7e6
Stephen Viens <sviens@apache.org>
2004-05-30 23:50:03 +0000

    Updating master Sybase ASE/jUDDI database setup script.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/trunk@262824 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sql/sybase/insert_publishers.sql
@@ -0,0 +1,6 @@
+USE juddi;
+
+-- *** SAMPLE PUBLISHER ***
+-- INSERT INTO PUBLISHER (PUBLISHER_ID,PUBLISHER_NAME,EMAIL_ADDRESS,ENABLED,ADMIN)
+-- VALUES ('jdoe','John Doe','john.doe@apache.org','true','true');
+
