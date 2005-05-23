5607474bc8386b2f2aef8c378c54e94337017494
Stephen Viens <sviens@apache.org>
2005-05-23 04:37:06 +0000

    Factored out any/all dependencies on Jakarta Commons DBCP, Pool and Collections.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/trunk@263094 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/juddi/util/jdbc/ConnectionManager.java
+++ src/java/org/apache/juddi/util/jdbc/ConnectionManager.java
@@ -21,18 +21,11 @@ import java.sql.SQLException;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
- 
-import org.apache.commons.dbcp.ConnectionFactory;
-import org.apache.commons.dbcp.DriverManagerConnectionFactory;
-import org.apache.commons.dbcp.PoolableConnectionFactory;
-import org.apache.commons.dbcp.PoolingDataSource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.commons.pool.impl.GenericObjectPool;
-
 import org.apache.juddi.registry.RegistryEngine;
 import org.apache.juddi.util.Config;
-import org.apache.juddi.util.Loader;
 
 
 /**
@@ -42,28 +35,6 @@ public class ConnectionManager
 {
   // private reference to the jUDDI logger
   private static Log log = LogFactory.getLog(ConnectionManager.class);
-
-  // DBCP DataSource property keys and default values
-  private static final String USE_CONNECTION_POOL_VALUE_KEY = "juddi.useConnectionPool";
-  private static final boolean DEFAULT_USE_CONNECTION_POOL_VALUE = false;
-  
-  private static final String JDBC_DRIVER_KEY = "juddi.jdbcDriver";
-  private static final String DEFAULT_JDBC_DRIVER = "com.mysql.jdbc.Driver";
-
-  private static final String JDBC_URL_KEY = "juddi.jdbcURL";
-  private static final String DEFAULT_JDBC_URL = "jdbc:mysql://localhost/juddi";
-
-  private static final String JDBC_USERNAME_KEY = "juddi.jdbcUser";
-  private static final String DEFAULT_JDBC_USERNAME = "juddi";
-
-  private static final String JDBC_PASSWORD_KEY = "juddi.jdbcPassword";
-  private static final String DEFAULT_JDBC_PASSWORD = "juddi";
-
-  private static final String JDBC_MAX_ACTIVITY_KEY = "juddi.maxActive";
-  private static final int DEFAULT_JDBC_MAX_ACTIVITY = 10;
-
-  private static final String JDBC_MAX_IDLE_KEY = "juddi.maxIdle";
-  private static final int DEFAULT_JDBC_MAX_IDLE = 5;
 
   // Shared jUDDI DataSource
   private static DataSource dataSource = null;
@@ -76,7 +47,7 @@ public class ConnectionManager
   {
     // Make sure we've got a DataSource first
     if (dataSource == null)
-      dataSource = getDataSource();
+      dataSource = lookupDataSource();
 
     Connection conn = null;
     if (dataSource != null)
@@ -88,102 +59,7 @@ public class ConnectionManager
   /**
    *
    */
-  private static synchronized DataSource getDataSource()
-    throws SQLException
-  {
-    if (dataSource != null)
-      return dataSource;
-
-    boolean useInternalConnectionPool = 
-      Config.getBooleanProperty(USE_CONNECTION_POOL_VALUE_KEY,
-          DEFAULT_USE_CONNECTION_POOL_VALUE);
-    
-    if (useInternalConnectionPool)
-    {
-      if (dataSource == null)
-        dataSource = createDataSource(); // use DBCP (jUDDI Managed Connection Pool)
-    }
-    else
-    {
-      if (dataSource == null)
-        dataSource = lookupDataSource(); // use JNDI (Container Managed Connection Pool)
-    }
-
-    return dataSource;
-  }
-
-  /**
-   *
-   */
-  private static DataSource createDataSource()
-    throws SQLException
-  {
-    // make sure we still need to create a DataSource.
-    if (dataSource != null)
-      return dataSource;
-
-    // grab the JDBC properties we'll need to setup the connection pool.
-    String dbcpDriver = Config.getStringProperty(JDBC_DRIVER_KEY,DEFAULT_JDBC_DRIVER);
-    String dbcpURL = Config.getStringProperty(JDBC_URL_KEY,DEFAULT_JDBC_URL);
-    String dbcpUser = Config.getStringProperty(JDBC_USERNAME_KEY,DEFAULT_JDBC_USERNAME);
-    String dbcpPassword = Config.getStringProperty(JDBC_PASSWORD_KEY,DEFAULT_JDBC_PASSWORD);
-    int dbcpMaxActive = Config.getIntProperty(JDBC_MAX_ACTIVITY_KEY,DEFAULT_JDBC_MAX_ACTIVITY);
-    int dbcpMaxIdle = Config.getIntProperty(JDBC_MAX_IDLE_KEY,DEFAULT_JDBC_MAX_IDLE);
-
-    log.info("Using DBCP to create a JDBC DataSource using properties: \n" +
-      "\tDriver:    " + dbcpDriver + "\n" +
-      "\tURL:       " + dbcpURL + "\n" +
-      "\tUser:      " + dbcpUser + "\n" +
-      "\tPassword:  " + dbcpPassword + "\n" +
-      "\tMaxActive: " + dbcpMaxActive + "\n" +
-      "\tMaxIdle:   " + dbcpMaxIdle + "\n");
-
-    // make sure the JDBC Driver is loaded
-    try {
-      Loader.getClassForName(dbcpDriver);
-    }
-    catch(ClassNotFoundException cnfex) {
-      throw new SQLException("Could not locate JDBC Driver '" +
-        dbcpDriver+"' in classpath: "+cnfex.getMessage());
-    }
-
-    // create an ObjectPool that serves as the actual
-    // pool of connections. We'll use a GenericObjectPool
-    // instance, although any ObjectPool implementation
-    // will suffice.
-    GenericObjectPool connPool = new GenericObjectPool(null);
-    connPool.setMaxActive(10);
-    connPool.setMaxIdle(10);
-
-    // create a ConnectionFactory that the pool will use
-    // to create Connections. We'll use the
-    // DriverManagerConnectionFactory, using the connect
-    // string passed in the command line arguments.
-    ConnectionFactory connFact =
-      new DriverManagerConnectionFactory(dbcpURL,dbcpUser,dbcpPassword);
-
-    // create the PoolableConnectionFactory, which wraps
-    // the "real" Connections created by the ConnectionFactory
-    // with the classes that implement the pooling functionality.
-    try {
-      PoolableConnectionFactory pcf =
-        new PoolableConnectionFactory(connFact,connPool,null,null,false,true);
-    }
-    catch (Exception ex) {
-      throw new SQLException(ex.getMessage());
-    }
-
-    // create the PoolingDriver itself, passing in the
-    // object pool we created.
-    PoolingDataSource dataSource = new PoolingDataSource(connPool);
-
-    return dataSource;
-  }
-
-  /**
-   *
-   */
-  private static DataSource lookupDataSource()
+  private static synchronized DataSource lookupDataSource()
     throws SQLException
   {
     // make sure we still need to lookup the DataSource
