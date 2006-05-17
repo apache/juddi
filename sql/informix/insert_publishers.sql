e4813e5f65e427b7ca94e4d13b5489c60e9a653c
Stephen Viens <sviens@apache.org>
2006-05-17 14:26:39 +0000

    Adding Informix DB script donated by Lichun(Jack) Zhu.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/trunk@407266 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sql/informix/insert_publishers.sql
@@ -0,0 +1,5 @@
+database juddi;
+
+-- *** SAMPLE PUBLISHER ***
+INSERT INTO PUBLISHER (PUBLISHER_ID,PUBLISHER_NAME,EMAIL_ADDRESS,IS_ENABLED,IS_ADMIN)
+ VALUES ('juddi','Lichun Zhu','zhu19@uwindsor.ca','true','true');
