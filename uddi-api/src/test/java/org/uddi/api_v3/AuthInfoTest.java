e824359bb4f16fabbdfdf2399d10ce5315a735ea
Kurt Stam <kstam@apache.org>
2008-07-23 15:06:21 +0000

    Adding assert
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/branches/v3_trunk@679108 13f79535-47bb-0310-9956-ffa450edef68

--- uddi-api/src/test/java/org/uddi/api_v3/AuthInfoTest.java
+++ uddi-api/src/test/java/org/uddi/api_v3/AuthInfoTest.java
@@ -24,6 +24,10 @@ public class AuthInfoTest {
 	private final static String EXPECTED_XML_FRAGMENT = "<fragment xmlns:ns3=\"urn:uddi-org:api_v3\" xmlns:ns2=\"http://www.w3.org/2000/09/xmldsig#\">\n"
                                                        +"    <ns3:authInfo>AuthInfo String</ns3:authInfo>\n"
                                                        +"</fragment>";
+	private final static String UTF8_WORD = "メインページ";
+	private final static String EXPECTED_UTF8_XML_FRAGMENT = "<fragment xmlns:ns3=\"urn:uddi-org:api_v3\" xmlns:ns2=\"http://www.w3.org/2000/09/xmldsig#\">\n"
+        +"    <ns3:authInfo>" + UTF8_WORD + "</ns3:authInfo>\n"
+        +"</fragment>";
 	/**
 	 * Testing going from object to XML using JAXB using a XML Fragment.
 	 */
@@ -51,7 +55,8 @@ public class AuthInfoTest {
 	/**
 	 * Unmarshall an xml fragment.
 	 */
-	@Test public void unmarshall()
+	@Test 
+	public void unmarshall()
 	{
 		try {
 			JAXBContext jaxbContext=JAXBContext.newInstance("org.uddi.api_v3");
@@ -60,6 +65,48 @@ public class AuthInfoTest {
 			JAXBElement<AuthToken> element = unMarshaller.unmarshal(new StreamSource(reader),AuthToken.class);
 			String infoString = element.getValue().getAuthInfo();
 			assertEquals("AuthInfo String", infoString);
+		} catch (JAXBException jaxbe) {
+			fail("No exception should be thrown");
+		}
+	}
+	/**
+	 * Test handling of utf8 characters
+	 */
+	@Test
+	public void marshallUTF8()
+	{
+		try {
+			JAXBContext jaxbContext=JAXBContext.newInstance("org.uddi.api_v3");
+			Marshaller marshaller = jaxbContext.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+			marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+			ObjectFactory factory = new ObjectFactory();
+			AuthToken authToken = factory.createAuthToken();
+			authToken.setAuthInfo(UTF8_WORD);
+			StringWriter writer = new StringWriter();
+			JAXBElement<AuthToken> element = new JAXBElement<AuthToken>(new QName("","fragment"),AuthToken.class,authToken);
+			marshaller.marshal(element,writer);
+			String actualXml=writer.toString();
+			assertEquals(EXPECTED_UTF8_XML_FRAGMENT, actualXml);
+		} catch (JAXBException jaxbe) {
+			fail("No exception should be thrown");
+		}
+	}
+	
+	/**
+	 * Test handling of utf8 characters
+	 */
+	@Test
+	public void unmarshallUTF8()
+	{
+		try {
+			JAXBContext jaxbContext=JAXBContext.newInstance("org.uddi.api_v3");
+			Unmarshaller unMarshaller = jaxbContext.createUnmarshaller();
+			StringReader reader = new StringReader(EXPECTED_UTF8_XML_FRAGMENT);
+			JAXBElement<AuthToken> utf8Element = unMarshaller.unmarshal(new StreamSource(reader),AuthToken.class);
+			String infoString = utf8Element.getValue().getAuthInfo();
+			assertEquals(UTF8_WORD, infoString);
 		} catch (JAXBException jaxbe) {
 			fail("No exception should be thrown");
 		}
