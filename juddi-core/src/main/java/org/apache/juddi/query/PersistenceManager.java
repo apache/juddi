2dd9c7e927d9b89b5d00cb5348c28ac78e6ee6f8
Thomas Cunningham <tcunning@apache.org>
2008-11-05 14:55:16 +0000

    JUDDI-132 Commit PersistenceManager class.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/branches/v3_trunk@711580 13f79535-47bb-0310-9956-ffa450edef68

---
+++ juddi-core/src/main/java/org/apache/juddi/query/PersistenceManager.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2001-2008 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.juddi.query;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.EntityManager;
+
+import javax.persistence.Query;
+import java.util.List;
+
+public class PersistenceManager {
+	public static final String PERSISTENCE_UNIT_NAME = "juddiDatabase";
+
+	private static final EntityManagerFactory emf;
+	
+	static {
+		try {
+			emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		}
+		catch (Throwable t) {
+			System.err.println("Initial entityManagerFactory creation failed:" + t);
+			throw new ExceptionInInitializerError(t);
+		}
+	}
+
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+}
