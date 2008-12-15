67de30f7c55761b4bf62068eab17cc907068f2ac
Jeffrey Faath <jfaath@apache.org>
2008-12-15 19:29:50 +0000

    fixing AuthInfoTest whereas reversal of namespace was causing test failure.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/branches/v3_trunk@726777 13f79535-47bb-0310-9956-ffa450edef68

--- uddi-ws/src/test/java/org/uddi/api_v3/AuthInfoTest.java
+++ uddi-ws/src/test/java/org/uddi/api_v3/AuthInfoTest.java
@@ -13,6 +13,7 @@ import javax.xml.transform.stream.StreamSource;
 
 import static junit.framework.Assert.fail;
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 import org.junit.Test;
 import org.uddi.api_v3.AuthToken;
@@ -21,11 +22,18 @@ import org.uddi.api_v3.ObjectFactory;
 
 public class AuthInfoTest {
 
-	private final static String EXPECTED_XML_FRAGMENT = "<fragment xmlns:ns3=\"urn:uddi-org:api_v3\" xmlns:ns2=\"http://www.w3.org/2000/09/xmldsig#\">\n"
+	private final static String EXPECTED_XML_FRAGMENT1 = "<fragment xmlns:ns2=\"http://www.w3.org/2000/09/xmldsig#\" xmlns:ns3=\"urn:uddi-org:api_v3\">\n"
                                                        +"    <ns3:authInfo>AuthInfo String</ns3:authInfo>\n"
                                                        +"</fragment>";
+	private final static String EXPECTED_XML_FRAGMENT2 = "<fragment xmlns:ns3=\"urn:uddi-org:api_v3\" xmlns:ns2=\"http://www.w3.org/2000/09/xmldsig#\">\n"
+        +"    <ns3:authInfo>AuthInfo String</ns3:authInfo>\n"
+        +"</fragment>";
 	private final static String UTF8_WORD = "メインページ";
-	private final static String EXPECTED_UTF8_XML_FRAGMENT = "<fragment xmlns:ns3=\"urn:uddi-org:api_v3\" xmlns:ns2=\"http://www.w3.org/2000/09/xmldsig#\">\n"
+
+	private final static String EXPECTED_UTF8_XML_FRAGMENT1 = "<fragment xmlns:ns2=\"http://www.w3.org/2000/09/xmldsig#\" xmlns:ns3=\"urn:uddi-org:api_v3\">\n"
+        +"    <ns3:authInfo>" + UTF8_WORD + "</ns3:authInfo>\n"
+        +"</fragment>";
+	private final static String EXPECTED_UTF8_XML_FRAGMENT2 = "<fragment xmlns:ns3=\"urn:uddi-org:api_v3\" xmlns:ns2=\"http://www.w3.org/2000/09/xmldsig#\">\n"
         +"    <ns3:authInfo>" + UTF8_WORD + "</ns3:authInfo>\n"
         +"</fragment>";
 	/**
@@ -47,7 +55,9 @@ public class AuthInfoTest {
 			JAXBElement<AuthToken> element = new JAXBElement<AuthToken>(new QName("","fragment"),AuthToken.class,authToken);
 			marshaller.marshal(element,writer);
 			String actualXml=writer.toString();
-			assertEquals(EXPECTED_XML_FRAGMENT, actualXml);
+			
+			boolean assertion = EXPECTED_XML_FRAGMENT1.equals(actualXml) || EXPECTED_XML_FRAGMENT2.equals(actualXml);
+			assertTrue(assertion);
 		} catch (JAXBException jaxbe) {
 			fail("No exception should be thrown");
 		}
@@ -61,7 +71,7 @@ public class AuthInfoTest {
 		try {
 			JAXBContext jaxbContext=JAXBContext.newInstance("org.uddi.api_v3");
 			Unmarshaller unMarshaller = jaxbContext.createUnmarshaller();
-			StringReader reader = new StringReader(EXPECTED_XML_FRAGMENT);
+			StringReader reader = new StringReader(EXPECTED_XML_FRAGMENT1);
 			JAXBElement<AuthToken> element = unMarshaller.unmarshal(new StreamSource(reader),AuthToken.class);
 			String infoString = element.getValue().getAuthInfo();
 			assertEquals("AuthInfo String", infoString);
@@ -88,7 +98,9 @@ public class AuthInfoTest {
 			JAXBElement<AuthToken> element = new JAXBElement<AuthToken>(new QName("","fragment"),AuthToken.class,authToken);
 			marshaller.marshal(element,writer);
 			String actualXml=writer.toString();
-			assertEquals(EXPECTED_UTF8_XML_FRAGMENT, actualXml);
+
+			boolean assertion = EXPECTED_UTF8_XML_FRAGMENT1.equals(actualXml) || EXPECTED_UTF8_XML_FRAGMENT2.equals(actualXml);
+			assertTrue(assertion);
 		} catch (JAXBException jaxbe) {
 			fail("No exception should be thrown");
 		}
@@ -103,7 +115,7 @@ public class AuthInfoTest {
 		try {
 			JAXBContext jaxbContext=JAXBContext.newInstance("org.uddi.api_v3");
 			Unmarshaller unMarshaller = jaxbContext.createUnmarshaller();
-			StringReader reader = new StringReader(EXPECTED_UTF8_XML_FRAGMENT);
+			StringReader reader = new StringReader(EXPECTED_UTF8_XML_FRAGMENT1);
 			JAXBElement<AuthToken> utf8Element = unMarshaller.unmarshal(new StreamSource(reader),AuthToken.class);
 			String infoString = utf8Element.getValue().getAuthInfo();
 			assertEquals(UTF8_WORD, infoString);
