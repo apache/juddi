70bf280868839cba1c47a712733f8e819f99a87d
Kurt Stam <kstam@apache.org>
2009-03-17 02:15:34 +0000

    JUDDI-201, adding license text
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/branches/v3_trunk@755078 13f79535-47bb-0310-9956-ffa450edef68

--- juddi-console/uddi-portlets/src/main/java/org/apache/juddi/portlets/server/InquiryServiceImpl.java
+++ juddi-console/uddi-portlets/src/main/java/org/apache/juddi/portlets/server/InquiryServiceImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2001-2009 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package org.apache.juddi.portlets.server;
 
 import java.util.HashMap;
@@ -16,7 +32,11 @@ import org.uddi.api_v3.client.transport.Transport;
 import org.uddi.v3_service.UDDIInquiryPortType;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-
+/**
+ * 
+ * @author <a href="mailto:kstam@apache.org">Kurt T Stam</a>
+ *
+ */
 public class InquiryServiceImpl extends RemoteServiceServlet implements InquiryService {
 
 	private Logger logger = Logger.getLogger(this.getClass());
