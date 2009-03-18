21f67d41942fa547ea5a90af38e13199390ce513
Kurt Stam <kstam@apache.org>
2009-03-18 17:01:49 +0000

    Adding portlet support
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/branches/v3_trunk@755649 13f79535-47bb-0310-9956-ffa450edef68

---
+++ juddi-console/uddi-portlets/src/main/java/org/apache/juddi/portlets/server/UDDIBrowserPortlet.java
@@ -0,0 +1,35 @@
+package org.apache.juddi.portlets.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.PortletSecurityException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+public class UDDIBrowserPortlet extends GenericPortlet 
+{
+	protected void doView(RenderRequest renderRequest, RenderResponse renderResponse) 
+		throws PortletException, PortletSecurityException, IOException
+	{
+		
+	  renderResponse.setContentType("text/html");
+	  PrintWriter writer = renderResponse.getWriter();
+	  writer.println("<script type='text/javascript' language='javascript' src='" 
+			  + renderRequest.getContextPath() 
+			  + "/org.apache.juddi.portlets.Application/org.apache.juddi.portlets.Application.nocache.js'></script>");
+	  writer.println("<div id='login'></div>");
+	  writer.close();
+	}
+
+	protected void doHelp(RenderRequest renderRequest, RenderResponse renderResponse) 
+		throws PortletException, PortletSecurityException, IOException
+	{
+	  renderResponse.setContentType("text/html");
+	  PrintWriter writer = renderResponse.getWriter();
+	  writer.write("Help");
+	  writer.close();
+	}
+}
