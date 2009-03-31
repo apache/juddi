aa24f341f681f0da3c5d79b4c21e458932d88617
Kurt Stam <kstam@apache.org>
2009-03-31 03:38:31 +0000

    JUDDI-201 adding description to language utility.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/branches/v3_trunk@760304 13f79535-47bb-0310-9956-ffa450edef68

--- uddi-client/src/main/java/org/uddi/api_v3/client/i18n/EntityForLang.java
+++ uddi-client/src/main/java/org/uddi/api_v3/client/i18n/EntityForLang.java
@@ -3,19 +3,14 @@ package org.uddi.api_v3.client.i18n;
 import java.util.List;
 
 import org.uddi.api_v3.Address;
+import org.uddi.api_v3.Description;
 import org.uddi.api_v3.Name;
-
+/** 
+ * Returns the entity in the proper language. If no language is specified
+ * or if no language is matched return the value of the first entity in the list.
+ */
 public class EntityForLang {
-	/** 
-	 * Returns the name in the proper language. If no language is specified
-	 * or if no language is matched return the value of the first name in the list.
-	 * 
-	 * @param nameList - List of names
-	 * @param lang - requested name.
-	 *
-	 * @return - the value of the name in the language matched.
-	 * 
-	 */
+	
 	public static Name get(List<Name> entityList, String lang) {
 		
 		if (lang==null) return entityList.get(0);
@@ -26,20 +21,22 @@ public class EntityForLang {
 		}
 		return entityList.get(0);
 	}
-	/** 
-	 * Returns the name in the proper language. If no language is specified
-	 * or if no language is matched return the value of the first name in the list.
-	 * 
-	 * @param nameList - List of names
-	 * @param lang - requested name.
-	 *
-	 * @return - the value of the name in the language matched.
-	 * 
-	 */
+	
 	public static Address get(List<Address> entityList, String lang) {
 		
 		if (lang==null) return entityList.get(0);
 		for (Address entity : entityList) {
+			if (lang.equalsIgnoreCase(entity.getLang())) {
+				return entity;
+			}
+		}
+		return entityList.get(0);
+	}
+	
+	public static Description get(List<Description> entityList, String lang) {
+		
+		if (lang==null) return entityList.get(0);
+		for (Description entity : entityList) {
 			if (lang.equalsIgnoreCase(entity.getLang())) {
 				return entity;
 			}
