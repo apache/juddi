7f55a9ae9856c29ade0aba9ef4f4eec774872836
Thomas Cunningham <tcunning@apache.org>
2009-05-04 04:50:23 +0000

    JUDDI-196 Initial commit of start of the Subscription Listener portlet.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/branches/v3_trunk@771174 13f79535-47bb-0310-9956-ffa450edef68

---
+++ juddi-console/uddi-portlets/src/main/java/org/apache/juddi/portlets/server/UDDISubscriptionNotificationPortlet.java
@@ -0,0 +1,34 @@
+package org.apache.juddi.portlets.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.PortletSecurityException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+public class UDDISubscriptionNotificationPortlet extends GenericPortlet 
+{
+	protected void doView(RenderRequest renderRequest, RenderResponse renderResponse) 
+		throws PortletException, PortletSecurityException, IOException
+	{		
+	  renderResponse.setContentType("text/html");
+	  PrintWriter writer = renderResponse.getWriter();
+	  writer.println("<script type='text/javascript' language='javascript' src='" 
+		  + renderRequest.getContextPath() 
+		  + "/org.apache.juddi.portlets.UDDISubscriptionNotification/org.apache.juddi.portlets.UDDISubscriptionNotification.nocache.js'></script>");
+	  writer.println("<div id=\"search\"></div>");
+	  writer.close();
+	}
+
+	protected void doHelp(RenderRequest renderRequest, RenderResponse renderResponse) 
+		throws PortletException, PortletSecurityException, IOException
+	{
+	  renderResponse.setContentType("text/html");
+	  PrintWriter writer = renderResponse.getWriter();
+	  writer.write("Help");
+	  writer.close();
+	}
+}
