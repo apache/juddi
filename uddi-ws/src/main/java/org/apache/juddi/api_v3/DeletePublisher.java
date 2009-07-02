efa97d1b83b401d1d0d7a5e58d31905148c2b482
Kurt Stam <kstam@apache.org>
2009-07-02 04:50:01 +0000

    JUDDI-259 adding JUDDIPublisherPortType
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/branches/v3_trunk@790459 13f79535-47bb-0310-9956-ffa450edef68

--- uddi-ws/src/main/java/org/apache/juddi/api_v3/DeletePublisher.java
+++ uddi-ws/src/main/java/org/apache/juddi/api_v3/DeletePublisher.java
@@ -24,6 +24,7 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -39,7 +40,9 @@ import javax.xml.bind.annotation.XmlType;
 })
 public class DeletePublisher implements Serializable{
 
-    protected String authInfo;
+	@XmlTransient
+	private static final long serialVersionUID = 1L;
+	protected String authInfo;
     @XmlElement(required = true)
     protected List<String> publisherId;
 
