a8e22693dff603552ea5f9880e6d0315fa4af482
Thomas Cunningham <tcunning@apache.org>
2009-09-12 17:43:53 +0000

    JUDDI-277 Add handler.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/trunk@814211 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uddi-client/src/main/java/org/uddi/api_v3/client/local/Handler.java
@@ -0,0 +1,29 @@
+package org.uddi.api_v3.client.local;
+
+public class Handler {
+	private String methodName;
+	private Class parameter;
+
+	public Handler() {
+		super();
+	}
+	
+	public Handler(String methodName, Class parameter) {
+		super();
+		this.methodName = methodName;
+		this.parameter = parameter;
+	}
+	
+	public String getMethodName() {
+		return methodName;
+	}
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+	public Class getParameter() {
+		return parameter;
+	}
+	public void setParameter(Class parameter) {
+		this.parameter = parameter;
+	}	
+}
