5f5507d75624a3570298eb14213dce6f6d86d2b8
Kurt Stam <kstam@apache.org>
2009-10-15 01:15:14 +0000

    JUDDI-240 working on subscriptions
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/trunk@825368 13f79535-47bb-0310-9956-ffa450edef68

--- juddi-console/uddi-portlets/src/main/java/org/apache/juddi/portlets/server/service/SubscriptionServiceImpl.java
+++ juddi-console/uddi-portlets/src/main/java/org/apache/juddi/portlets/server/service/SubscriptionServiceImpl.java
@@ -298,6 +298,7 @@ public class SubscriptionServiceImpl extends RemoteServiceServlet implements Sub
 		getSubscriptionResults.setSubscriptionKey(modelSubscription.getSubscriptionKey());
 		//getSubscriptionResults.set
 		
+		
 		return response;
 	}
 
