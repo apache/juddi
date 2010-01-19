4d33427f99733e143a603da5e938983413193e7e
Thomas Cunningham <tcunning@apache.org>
2010-01-19 18:27:28 +0000

    JUDDI-326 Add headers.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/juddi/trunk@900888 13f79535-47bb-0310-9956-ffa450edef68

--- juddi-core/src/main/java/org/apache/juddi/ClassUtil.java
+++ juddi-core/src/main/java/org/apache/juddi/ClassUtil.java
@@ -1,4 +1,24 @@
+/*
+ * Copyright 2001-2010 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package org.apache.juddi;
+
+/**
+ * @author <a href="mailto:tcunning@apache.org">Tom Cunningham</a>
+ */
 
 public class ClassUtil {
     public static Class forName(String name, Class caller)
