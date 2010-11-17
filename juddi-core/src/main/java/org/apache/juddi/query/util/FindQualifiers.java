2b41a9f5d4e6bb81b7804f881b9b3961692a9de9
Kurt Stam <kstam@apache.org>
2010-11-17 03:43:15 +0000

    JUDDI-427 applying patch provided by Lyudmil Latinov. Referencing the tModelKeys rather then the names.
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1035915 13f79535-47bb-0310-9956-ffa450edef68

--- juddi-core/src/main/java/org/apache/juddi/query/util/FindQualifiers.java
+++ juddi-core/src/main/java/org/apache/juddi/query/util/FindQualifiers.java
@@ -25,70 +25,70 @@ import java.util.List;
 public class FindQualifiers {
 
 	public static final String AND_ALL_KEYS = "andAllKeys";
-	public static final String AND_ALL_KEYS_TMODEL = "uddi-org:andAllKeys";
+	public static final String AND_ALL_KEYS_TMODEL = "uddi:uddi.org:findqualifier:andallkeys";
 
 	public static final String APPROXIMATE_MATCH = "approximateMatch";
-	public static final String APPROXIMATE_MATCH_TMODEL = "uddi-org:approximateMatch:SQL99";
+	public static final String APPROXIMATE_MATCH_TMODEL = "uddi:uddi.org:findqualifier:approximatematch";
 
 	public static final String BINARY_SORT = "binarySort";
-	public static final String BINARY_SORT_TMODEL = "uddi-org:binarySort)";
+	public static final String BINARY_SORT_TMODEL = "uddi:uddi.org:sortorder:binarysort";
 
 	public static final String BINDING_SUBSET = "bindingSubset";
-	public static final String BINDING_SUBSET_TMODEL = "uddi-org:bindingSubset";
+	public static final String BINDING_SUBSET_TMODEL = "uddi:uddi.org:findqualifier:bindingsubset";
 
 	public static final String CASE_INSENSITIVE_SORT = "caseInsensitiveSort";
-	public static final String CASE_INSENSITIVE_SORT_TMODEL = "uddi-org:caseInsensitiveSort";
+	public static final String CASE_INSENSITIVE_SORT_TMODEL = "uddi:uddi.org:findqualifier:caseinsensitivesort";
 
 	public static final String CASE_INSENSITIVE_MATCH = "caseInsensitiveMatch";
-	public static final String CASE_INSENSITIVE_MATCH_TMODEL = "uddi-org:caseInsensitiveMatch";
+	public static final String CASE_INSENSITIVE_MATCH_TMODEL = "uddi:uddi.org:findqualifier:caseinsensitivematch";
 
 	public static final String CASE_SENSITIVE_SORT = "caseSensitiveSort";
-	public static final String CASE_SENSITIVE_SORT_TMODEL = "uddi-org:caseSensitiveSort";
+	public static final String CASE_SENSITIVE_SORT_TMODEL = "uddi:uddi.org:findqualifier:casesensitivesort";
 
 	public static final String CASE_SENSITIVE_MATCH = "caseSensitiveMatch";
-	public static final String CASE_SENSITIVE_MATCH_TMODEL = "uddi-org:caseSensitiveMatch";
+	public static final String CASE_SENSITIVE_MATCH_TMODEL = "uddi:uddi.org:findqualifier:casesensitivematch";
 
 	public static final String COMBINE_CATEGORY_BAGS = "combineCategoryBags";
-	public static final String COMBINE_CATEGORY_BAGS_TMODEL = "uddi-org:combineCategoryBags)";
+	public static final String COMBINE_CATEGORY_BAGS_TMODEL = "uddi:uddi.org:findqualifier:combinecategorybags";
 
 	public static final String DIACRITIC_INSENSITIVE_MATCH = "diacriticInsensitiveMatch";
-	public static final String DIACRITIC_INSENSITIVE_MATCH_TMODEL = "uddi-org:diacriticInsensitiveMatch";
+	public static final String DIACRITIC_INSENSITIVE_MATCH_TMODEL = "uddi:uddi.org:findqualifier:diacriticsinsensitivematch";
 
 	public static final String DIACRITIC_SENSITIVE_MATCH = "diacriticSensitiveMatch";
-	public static final String DIACRITIC_SENSITIVE_MATCH_TMODEL = "uddi-org:diacriticSensitiveMatch";
+	public static final String DIACRITIC_SENSITIVE_MATCH_TMODEL = "uddi:uddi.org:findqualifier:diacriticssensitivematch";
 
 	public static final String EXACT_MATCH = "exactMatch";
-	public static final String EXACT_MATCH_TMODEL = "uddi-org:exactMatch";
+	public static final String EXACT_MATCH_TMODEL = "uddi:uddi.org:findqualifier:exactmatch";
 
 	public static final String SIGNATURE_PRESENT = "signaturePresent";
-	public static final String SIGNATURE_PRESENT_TMODEL = "uddi-org:signaturePresent";
+	public static final String SIGNATURE_PRESENT_TMODEL = "uddi:uddi.org:findqualifier:signaturepresent";
 
 	public static final String OR_ALL_KEYS = "orAllKeys";
-	public static final String OR_ALL_KEYS_TMODEL = "uddi-org:orAllKeys";
+	public static final String OR_ALL_KEYS_TMODEL = "uddi:uddi.org:findqualifier:orallkeys";
 
 	public static final String OR_LIKE_KEYS = "orLikeKeys";
-	public static final String OR_LIKE_KEYS_TMODEL = "uddi-org:orLikeKeys";
+	public static final String OR_LIKE_KEYS_TMODEL = "uddi:uddi.org:findqualifier:orlikekeys";
 
 	public static final String SERVICE_SUBSET = "serviceSubset";
-	public static final String SERVICE_SUBSET_TMODEL = "uddi-org:serviceSubset";
+	public static final String SERVICE_SUBSET_TMODEL = "uddi:uddi.org:findqualifier:servicesubset";
 
 	public static final String SORT_BY_NAME_ASC = "sortByNameAsc";
-	public static final String SORT_BY_NAME_ASC_TMODEL = "uddi-org:sortByNameAsc";
+	public static final String SORT_BY_NAME_ASC_TMODEL = "uddi:uddi.org:findqualifier:sortbynameasc";
 
 	public static final String SORT_BY_NAME_DESC = "sortByNameDesc";
-	public static final String SORT_BY_NAME_DESC_TMODEL = "uddi-org:sortByNameDesc";
+	public static final String SORT_BY_NAME_DESC_TMODEL = "uddi:uddi.org:findqualifier:sortbynamedesc";
 
 	public static final String SORT_BY_DATE_ASC = "sortByDateAsc";
-	public static final String SORT_BY_DATE_ASC_TMODEL = "uddi-org:sortByDateAsc";
+	public static final String SORT_BY_DATE_ASC_TMODEL = "uddi:uddi.org:findqualifier:sortbydateasc";
 	
 	public static final String SORT_BY_DATE_DESC = "sortByDateDesc";
-	public static final String SORT_BY_DATE_DESC_TMODEL = "uddi-org:sortByDateDesc";
+	public static final String SORT_BY_DATE_DESC_TMODEL = "uddi:uddi.org:findqualifier:sortbydatedesc";
 
 	public static final String SUPPRESS_PROJECTED_SERVICES = "suppressProjectedServices";
-	public static final String SUPPRESS_PROJECTED_SERVICES_TMODEL = "uddi-org:suppressProjectedServices";
+	public static final String SUPPRESS_PROJECTED_SERVICES_TMODEL = "uddi:uddi.org:findqualifier:suppressprojectedservices";
 
 	public static final String UTS_10 = "UTS-10";
-	public static final String UTS_10_TMODEL = "uddi-org:UTS-10";
+	public static final String UTS_10_TMODEL = "uddi:uddi.org:sortorder:uts-10";
 	
 	private boolean andAllKeys = false;
 	private boolean approximateMatch = false;
