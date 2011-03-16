0bca0fb2c86c90f01b917206b18e58ef009d8c51
Kurt Stam <kstam@apache.org>
2011-03-16 01:05:05 +0000

    JUDDI-243 add support for RMI notification
    adding info logging to debug issue on buildbot
    
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1082022 13f79535-47bb-0310-9956-ffa450edef68

--- juddi-core/src/main/java/org/apache/juddi/api/impl/InquiryHelper.java
+++ juddi-core/src/main/java/org/apache/juddi/api/impl/InquiryHelper.java
@@ -337,14 +337,14 @@ public class InquiryHelper {
 			org.uddi.api_v3.ServiceInfo apiServiceInfo = new org.uddi.api_v3.ServiceInfo();
 			
 			logger.info(modelBusinessService.getEntityKey() + " is modified " + modelBusinessService.getModifiedIncludingChildren() + " " + modelBusinessService.getModifiedIncludingChildren().getTime() );
-			if (modifiedAfter != null && modifiedAfter.after(modelBusinessService.getModifiedIncludingChildren())) {
+			if (modifiedAfter != null && modifiedAfter.getTime() > (modelBusinessService.getModifiedIncludingChildren().getTime())) {
 				currentIndex++;
 				logger.info(modifiedAfter.getTime() + " " + modelBusinessService.getModifiedIncludingChildren().getTime());
 				logger.info("after");
 				continue;
 			}
 			
-			if (modifiedBefore != null && modifiedBefore.before(modelBusinessService.getModifiedIncludingChildren())) {
+			if (modifiedBefore != null && modifiedBefore.getTime() < (modelBusinessService.getModifiedIncludingChildren().getTime())) {
 				currentIndex++;
 				logger.info(modifiedBefore.getTime() + " " + modelBusinessService.getModifiedIncludingChildren().getTime());
 				logger.info("before");
