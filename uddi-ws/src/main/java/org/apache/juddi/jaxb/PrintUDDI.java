6bf3a03321929a8d886644ea6440cecc3d817489
Kurt Stam <kstam@apache.org>
2011-05-13 12:56:57 +0000

    JUDDI-476 can be used for logging/debug purposes
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1102702 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uddi-ws/src/main/java/org/apache/juddi/jaxb/PrintUDDI.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2001-2009 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.juddi.jaxb;
+
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
+
+import org.uddi.api_v3.BindingTemplate;
+import org.uddi.api_v3.FindTModel;
+
+
+/**
+ * @author <a href="mailto:kstam@apache.org">Kurt T Stam</a>
+ * @param <T>
+ */
+public class PrintUDDI<T> {
+
+	static JAXBContext jaxbContext = null;
+	
+	private Marshaller getUDDIMarshaller() throws JAXBException {
+		if (jaxbContext==null) {
+			jaxbContext=JAXBContext.newInstance("org.uddi.api_v3");
+		}
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+		marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+		
+		return marshaller;
+	}
+	
+	public String print(T object, Class<T> type) {
+		String xml = "";
+		try {
+			StringWriter writer = new StringWriter();
+			JAXBElement<T> element = new JAXBElement<T>(new QName("",object.getClass().getName()),type,object);
+			getUDDIMarshaller().marshal(element,writer);
+			xml=writer.toString();
+		} catch (JAXBException je) {
+			
+		}
+		return xml;
+	}
+
+}
