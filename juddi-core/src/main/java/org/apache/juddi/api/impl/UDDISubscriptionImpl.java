c694e40d40614fd5a96e3ed97fe4f43cc3a51516
Kurt Stam <kstam@apache.org>
2011-06-10 15:30:58 +0000

    JUDDI-495, toning down logging
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1134342 13f79535-47bb-0310-9956-ffa450edef68

--- juddi-core/src/main/java/org/apache/juddi/api/impl/UDDISubscriptionImpl.java
+++ juddi-core/src/main/java/org/apache/juddi/api/impl/UDDISubscriptionImpl.java
@@ -311,8 +311,7 @@ public class UDDISubscriptionImpl extends AuthenticatedService implements UDDISu
 			if (subscriptionFilter.getFindService() != null) {
 				//Get the current matching keys
 				List<?> currentMatchingKeys = getSubscriptionMatches(subscriptionFilter, em);
-				//if (logger.isDebugEnabled()) 
-				logger.info("current matching keys=" + currentMatchingKeys);
+				if (logger.isDebugEnabled()) logger.debug("current matching keys=" + currentMatchingKeys);
 				List<String> missingKeys = getMissingKeys(currentMatchingKeys, modelSubscription.getSubscriptionMatches());
 				if (missingKeys != null && missingKeys.size() > 0) {
 					KeyBag missingKeyBag = new KeyBag();
