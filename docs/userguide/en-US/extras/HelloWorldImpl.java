984383a4d9cb68e3e2305e24a3c0ae76c30828a3
Kurt Stam <kstam@apache.org>
2011-08-04 18:29:30 +0000

    JUDDI-246 working on juddi-client documentation
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1153958 13f79535-47bb-0310-9956-ffa450edef68

--- docs/userguide/en-US/extras/HelloWorldImpl.java
+++ docs/userguide/en-US/extras/HelloWorldImpl.java
@@ -4,19 +4,11 @@ import javax.jws.WebService;
 import org.apache.juddi.v3.annotations.UDDIService;
 import org.apache.juddi.v3.annotations.UDDIServiceBinding;
 
-@UDDIService(
-		businessKey="uddi:myBusinessKey",
-		serviceKey="uddi:myServiceKey", 
-		description = "Hello World test service")
-@UDDIServiceBinding(
-		bindingKey="uddi:myServiceBindingKey",
-	    description="WSDL endpoint for the helloWorld Service. This service is used for "
-				  + "testing the jUDDI annotation functionality",
-	    accessPointType="wsdlDeployment",
-	    accessPoint="http://localhost:8080/juddiv3-samples/services/helloworld?wsdl")
-@WebService(
-		endpointInterface = "org.apache.juddi.samples.HelloWorld",
-        serviceName = "HelloWorld")
+@UDDIService(businessKey="uddi:myBusinessKey", serviceKey="uddi:myServiceKey", description = "Hello World test service")
+@UDDIServiceBinding(bindingKey="uddi:myServiceBindingKey", description="WSDL endpoint for the helloWorld Service. This service is used for "
+      + "testing the jUDDI annotation functionality",
+    accessPointType="wsdlDeployment", accessPoint="http://localhost:8080/juddiv3-samples/services/helloworld?wsdl")
+@WebService(endpointInterface = "org.apache.juddi.samples.HelloWorld", serviceName = "HelloWorld")
 
 public class HelloWorldImpl implements HelloWorld {
     public String sayHi(String text) {
