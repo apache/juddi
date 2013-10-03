a609d71c2aca4140b7ef1a0589b9b68560b2d8e3
alexoree <alexoree@unknown>
2013-10-03 02:30:22 +0000

    JUDDI-567 revising bb-win7 test script, this time exit codes will be returned for correct test failure identification by buildbot
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1528706 13f79535-47bb-0310-9956-ffa450edef68

--- juddi-client.net/bb-win7.bat
+++ juddi-client.net/bb-win7.bat
@@ -1,5 +1,8 @@
-rem Hi, this is for running nunit tests on ASF's build bot CI servers
+@echo off
+rem Hi, this is for running nunit tests on ASF's build bot CI servers, specifically the Windows 7 bb server
+
+set errorlevel=
 
 "C:\Program Files\NUnit 2.6.2\bin\nunit-console.exe" juddi-client.net\juddi-client.net.test\bin\Debug\juddi-client.net.test.dll
-
+exit /b %errorlevel%
 rem c:\juddi\NUnit-2.6.1\bin\nunit-console.exe juddi-client.net\juddi-client.net.test\bin\Debug\juddi-client.net.test.dll
