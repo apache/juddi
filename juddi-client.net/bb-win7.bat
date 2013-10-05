635a9583b69c574d36696e77f4ca96288b823d5b
alexoree <alexoree@unknown>
2013-10-05 22:29:35 +0000

    JUDDI-567 new integration tests, revising script
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1529541 13f79535-47bb-0310-9956-ffa450edef68

--- juddi-client.net/bb-win7.bat
+++ juddi-client.net/bb-win7.bat
@@ -1,7 +1,16 @@
 @echo off
 rem Hi, this is for running nunit tests on ASF's build bot CI servers, specifically the Windows 7 bb server
+if "%NUNIT_HOME%"=="" (
+	set NUNIT_HOME=c:\juddi\NUnit-2.6.1
+	echo WARN - NUNIT_HOME is not defined, using default value, which is probably wrong
+)
+if not exist %NUNIT_HOME%   (
+	echo The path %NUNIT_HOME% does not exist, download Nunit and setup the environment variable
+	goto exit
+)
+echo Using %NUNIT_HOME% for NUNIT location
 
 set errorlevel=
-c:\juddi\NUnit-2.6.1\bin\nunit-console.exe juddi-client.net\juddi-client.net.test\bin\Debug\juddi-client.net.test.dll
-rem "C:\Program Files\NUnit 2.6.2\bin\nunit-console.exe" juddi-client.net\juddi-client.net.test\bin\Debug\juddi-client.net.test.dll
+rem c:\juddi\NUnit-2.6.1\bin\nunit-console.exe juddi-client.net\juddi-client.net.test\bin\Debug\juddi-client.net.test.dll
+"%NUNIT_HOME%\bin\nunit-console.exe" juddi-client.net\juddi-client.net.test\bin\Debug\juddi-client.net.test.dll
 exit /b %errorlevel%
