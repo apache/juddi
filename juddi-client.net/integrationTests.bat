f6c66fb403a33d014534f939c93ceb41d8b31768
alexoree <alexoree@unknown>
2013-10-05 23:28:35 +0000

    JUDDI-567 new integration tests, revising script
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1529550 13f79535-47bb-0310-9956-ffa450edef68

--- juddi-client.net/integrationTests.bat
+++ juddi-client.net/integrationTests.bat
@@ -3,7 +3,7 @@ if "%NUNIT_HOME%"=="" (
 	set NUNIT_HOME=C:\juddi\NUnit-2.6.1
 	echo WARN - NUNIT_HOME is not defined, using default value, which is probably wrong
 )
-if not exist %NUNIT_HOME%   (
+if not exist "%NUNIT_HOME%"   (
 	echo The path %NUNIT_HOME% does not exist, download Nunit and setup the environment variable
 	goto exit
 )
@@ -31,5 +31,9 @@ call shutdown.bat
 cd ..\..\..\..\..
 echo Exit code %exitcode%
 exit /b %exitcode%
+goto ok
 
 :exit
+exit /b 1
+
+:ok
