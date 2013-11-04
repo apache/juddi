da5a949b6c7d0fa088aad7db5926f7e8e4c5c193
alexoree <alexoree@unknown>
2013-11-04 01:34:41 +0000

    JUDDI-567 missed a file on checkin
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1538486 13f79535-47bb-0310-9956-ffa450edef68

---
+++ juddi-client.net/juddi-client.net/org.apache.juddi.v3.client.mapping/ServiceRegistrationResponse.cs
@@ -0,0 +1,34 @@
+﻿using org.uddi.apiv3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace org.apache.juddi.v3.client.mapping
+{
+    public class ServiceRegistrationResponse
+    {
+
+        private String bindingKey;
+        private businessService businessService;
+
+        public String getBindingKey()
+        {
+            return bindingKey;
+        }
+        public void setBindingKey(String bindingKey)
+        {
+            this.bindingKey = bindingKey;
+        }
+        public businessService getBusinessService()
+        {
+            return businessService;
+        }
+        public void setBusinessService(businessService businessService)
+        {
+            this.businessService = businessService;
+        }
+
+    }
+
+}
