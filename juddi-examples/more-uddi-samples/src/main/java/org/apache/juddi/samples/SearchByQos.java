e2a278cf3c9ec477a99e6ac9262ca9e9e764ba03
alexoree <alexoree@unknown>
2013-11-26 02:56:30 +0000

    JUDDI-633 adding example code to find by binding with qos parameters by binding and by service
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1545520 13f79535-47bb-0310-9956-ffa450edef68

--- juddi-examples/more-uddi-samples/src/main/java/org/apache/juddi/samples/SearchByQos.java
+++ juddi-examples/more-uddi-samples/src/main/java/org/apache/juddi/samples/SearchByQos.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.juddi.samples;
 
 import java.util.Properties;
@@ -79,8 +78,80 @@ public class SearchByQos {
     private static UDDIPublicationPortType publish = null;
     private static UDDIInquiryPortType inquiry;
 
-    public static void main(String[] args) throws Exception {
+    static void doFindService() throws Exception {
+                // create a manager and read the config in the archive; 
+        // you can use your config file name
+        UDDIClient clerkManager = new UDDIClient("META-INF/simple-publish-uddi.xml");
+        UDDIClerk clerk = clerkManager.getClerk("default");
+        // register the clerkManager with the client side container
+        UDDIClientContainer.addClient(clerkManager);            // a ClerkManager can be a client to multiple UDDI nodes, so 
+        // a ClerkManager can be a client to multiple UDDI nodes, so 
+        // supply the nodeName (defined in your uddi.xml.
+        // The transport can be WS, inVM, RMI etc which is defined in the uddi.xml
+        Transport transport = clerkManager.getTransport("default");
+        // Now you create a reference to the UDDI API
+        security = transport.getUDDISecurityService();
+        publish = transport.getUDDIPublishService();
+        inquiry = transport.getUDDIInquiryService();
+        //step one, get a token
+        GetAuthToken getAuthTokenRoot = new GetAuthToken();
+        getAuthTokenRoot.setUserID("uddi");
+        getAuthTokenRoot.setCred("uddi");
 
+        // Making API call that retrieves the authentication token for the 'root' user.
+        //String rootAuthToken = clerk.getAuthToken(clerk.getUDDINode().getSecurityUrl());
+        String uddi = security.getAuthToken(getAuthTokenRoot).getAuthInfo();
+
+        System.out.println("killing mary's business if it exists");
+        //first check is Mary's business exists and delete
+        DeleteIfExists("uddi:uddi.marypublisher.com:marybusinessone", uddi);
+
+        System.out.println("making mary's tmodel key gen");
+        //make the key gen since our test case uses some custom keys
+        TModel createKeyGenator = UDDIClerk.createKeyGenator("uddi.marypublisher.com", "mary key gen", "en");
+        //clerk.register(createKeyGenator);
+        System.out.println("saving...");
+        SaveTM(createKeyGenator, uddi);
+
+
+        System.out.println("fetching business list");
+        ServiceList before = getServiceList(uddi);
+        if (before.getServiceInfos()== null) {
+            System.out.println("before no service returned!");
+             before.setServiceInfos(new ServiceInfos());
+        } else {
+            System.out.println(before.getServiceInfos().getServiceInfo().size() + " service returned before");
+        }
+
+        System.out.println("saving mary");
+        SaveMary(uddi);
+
+        ServiceList after = getServiceList(uddi);
+        if (after.getServiceInfos().getServiceInfo()== null) {
+            System.out.println("after no service returned!");
+            after.setServiceInfos(new ServiceInfos());
+        } else {
+            System.out.println(after.getServiceInfos().getServiceInfo().size() + " service returned after");
+        }
+        PrintUDDI<ServiceList> p = new PrintUDDI<ServiceList>();
+        if (before.getServiceInfos().getServiceInfo().size()
+                < after.getServiceInfos().getServiceInfo().size()) {
+            System.out.println("hey it worked as advertised, double checking");
+            /*if (CheckFor(after, "uddi:uddi.marypublisher.com:marybusinessone")) {
+             System.out.println("ok!");
+             } else {
+             System.out.println("no good!");
+             }*/
+        } else {
+
+            System.out.println("something's not right, here's the before service listing");
+            System.out.println(p.print(before));
+            System.out.println(p.print(after));
+        }
+
+    }
+
+    static void doFindBinding() throws Exception{
         // create a manager and read the config in the archive; 
         // you can use your config file name
         UDDIClient clerkManager = new UDDIClient("META-INF/simple-publish-uddi.xml");
@@ -118,9 +189,9 @@ public class SearchByQos {
 
         System.out.println("fetching business list");
         BindingDetail before = getBindingList(uddi);
-        if (before.getBindingTemplate()== null) {
+        if (before.getBindingTemplate() == null) {
             System.out.println("before no service returned!");
-           // before.setServiceInfos(new ServiceInfos());
+            // before.setServiceInfos(new ServiceInfos());
         } else {
             System.out.println(before.getBindingTemplate().size() + " service returned before");
         }
@@ -129,7 +200,7 @@ public class SearchByQos {
         SaveMary(uddi);
 
         BindingDetail after = getBindingList(uddi);
-        if (after.getBindingTemplate()== null) {
+        if (after.getBindingTemplate() == null) {
             System.out.println("after no service returned!");
             //after.setServiceInfos(new ServiceInfos());
         } else {
@@ -140,10 +211,10 @@ public class SearchByQos {
                 < after.getBindingTemplate().size()) {
             System.out.println("hey it worked as advertised, double checking");
             /*if (CheckFor(after, "uddi:uddi.marypublisher.com:marybusinessone")) {
-                System.out.println("ok!");
-            } else {
-                System.out.println("no good!");
-            }*/
+             System.out.println("ok!");
+             } else {
+             System.out.println("no good!");
+             }*/
         } else {
 
             System.out.println("something's not right, here's the before service listing");
@@ -151,6 +222,12 @@ public class SearchByQos {
             System.out.println(p.print(after));
         }
 
+    }
+
+    public static void main(String[] args) throws Exception {
+        doFindBinding();
+        
+        doFindService();
     }
 
     private static void DeleteIfExists(String key, String authInfo) {
@@ -179,41 +256,33 @@ public class SearchByQos {
         }
     }
 
-    
-     private static ServiceList getServiceList(String token) throws Exception {
+    private static ServiceList getServiceList(String token) throws Exception {
         FindService fb = new FindService();
         fb.setAuthInfo(token);
         org.uddi.api_v3.FindQualifiers fq = new org.uddi.api_v3.FindQualifiers();
         fq.getFindQualifier().add(UDDIConstants.APPROXIMATE_MATCH);
         fb.setFindQualifiers(fq);
-        //fb.getName().add((new Name(UDDIConstants.WILDCARD, null)));
-        
-        fb.setCategoryBag(new CategoryBag());
-        fb.getCategoryBag().getKeyedReference().add(
-                new KeyedReference(WSDMQosConstants.METRIC_FAULT_COUNT_KEY, WSDMQosConstants.METRIC_FaultCount, "400"));
-        
+        fb.getName().add((new Name(UDDIConstants.WILDCARD, null)));
+
+        fb.setTModelBag(new TModelBag());
+        fb.getTModelBag().getTModelKey().add(WSDMQosConstants.METRIC_FAULT_COUNT_KEY);
         return inquiry.findService(fb);
     }
-     
-         
-     private static BindingDetail getBindingList(String token) throws Exception {
+
+    private static BindingDetail getBindingList(String token) throws Exception {
         FindBinding fb = new FindBinding();
-        
-        
         fb.setAuthInfo(token);
-        org.uddi.api_v3.FindQualifiers fq = new org.uddi.api_v3.FindQualifiers();
-        fq.getFindQualifier().add(UDDIConstants.APPROXIMATE_MATCH);
-       // fb.setFindQualifiers(fq);
-        
         fb.setTModelBag(new TModelBag());
         fb.getTModelBag().getTModelKey().add(WSDMQosConstants.METRIC_FAULT_COUNT_KEY);
         return inquiry.findBinding(fb);
     }
 
     /**
-     * adds a business, service, bt with tmodel instance details with qos parameters
+     * adds a business, service, bt with tmodel instance details with qos
+     * parameters
+     *
      * @param rootAuthToken
-     * @throws Exception 
+     * @throws Exception
      */
     private static void SaveMary(String rootAuthToken) throws Exception {
         BusinessEntity be = new BusinessEntity();
@@ -235,14 +304,14 @@ public class SearchByQos {
         BusinessService bs = new BusinessService();
         bs.setBusinessKey("uddi:uddi.marypublisher.com:marybusinessone");
         bs.setServiceKey("uddi:uddi.marypublisher.com:marybusinessoneservice");
-        bs.getName().add(new Name("name!","en"));
+        bs.getName().add(new Name("name!", "en"));
         bs.setBindingTemplates(new BindingTemplates());
         BindingTemplate bt = new BindingTemplate();
         bt.setAccessPoint(new AccessPoint("http://localhost", AccessPointType.WSDL_DEPLOYMENT.toString()));
         bt.setTModelInstanceDetails(new TModelInstanceDetails());
         TModelInstanceInfo tii = new TModelInstanceInfo();
         tii.setTModelKey(WSDMQosConstants.METRIC_FAULT_COUNT_KEY);
-        
+
         tii.setInstanceDetails(new InstanceDetails());
         tii.getInstanceDetails().setInstanceParms("400");
         bt.getTModelInstanceDetails().getTModelInstanceInfo().add(tii);
