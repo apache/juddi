f71dc8f2634bc126e90a3d27a3e723f346202192
Kurt Stam <kstam@apache.org>
2013-12-27 00:32:43 +0000

    JUDDI-623 updating the wsdl and correcting the java code.
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1553611 13f79535-47bb-0310-9956-ffa450edef68

--- uddi-ws/src/main/java/org/apache/juddi/api_v3/SaveClerk.java
+++ uddi-ws/src/main/java/org/apache/juddi/api_v3/SaveClerk.java
@@ -35,7 +35,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "save_clerkInfo", propOrder = {
+@XmlType(name = "save_clerk", propOrder = {
 	"authInfo",
     "clerk"
 })
