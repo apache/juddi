6b98c2ef45a4057a22655e11e6bddebde29dfd57
alexoree <alexoree@unknown>
2014-01-10 11:59:18 +0000

    JUDDI-776 fixed for .net, tests added
    git-svn-id: https://svn.apache.org/repos/asf/juddi/trunk@1557099 13f79535-47bb-0310-9956-ffa450edef68

---
+++ juddi-client.net/juddi-client.net/org.apache.juddi.v3.client.mapping/Common2UDDI.cs
@@ -0,0 +1,48 @@
+﻿using org.apache.juddi.v3.client;
+using org.apache.juddi.v3.client.log;
+using org.uddi.apiv3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace org.apache.juddi.client.org.apache.juddi.v3.client.mapping
+{
+    public static class Common2UDDI
+    {
+        public static List<description> mapdescription(String content, String lang)
+        {
+
+            List<description> ret = new List<description>();
+            if (String.IsNullOrEmpty(content))
+            {
+                return ret;
+            }
+            if (content.Length > UDDIConstants.MAX_description_length)
+            {
+                int offset = 0;
+                while (offset < content.Length)
+                {
+                    description description = new description();
+                    description.lang = (lang);
+                    int len =  UDDIConstants.MAX_description_length;
+                    if (len > content.Length-offset)
+                    {
+                        len = content.Length -offset;
+                    }
+
+                    description.Value = (content.Substring(offset, len));
+                    offset = offset + UDDIConstants.MAX_description_length;
+                    ret.Add(description);
+
+                }
+            }
+            else
+            {
+                ret.Add(new description(content, lang));
+            }
+            return ret;
+
+        }
+    }
+}
