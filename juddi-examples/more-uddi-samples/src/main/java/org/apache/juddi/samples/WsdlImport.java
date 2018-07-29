6056b6cdfe1c1cebbe3059488be5d99d99dc80b0
Jimmy Casey <casey.jimmy@gmail.com>
2018-07-29 21:46:54 +0000

    Fixed Spelling.

--- juddi-examples/more-uddi-samples/src/main/java/org/apache/juddi/samples/WsdlImport.java
+++ juddi-examples/more-uddi-samples/src/main/java/org/apache/juddi/samples/WsdlImport.java
@@ -89,7 +89,7 @@ public class WsdlImport {
                 //step two, identify the key used for all your stuff
                 //you must have a key generator created already
                 //here, we are assuming that you don't have one
-                //NOTE: these are some of the publically available WSDLs that were used to test WSDL2UDDI
+                //NOTE: these are some of the publicly available WSDLs that were used to test WSDL2UDDI
                 //URL url = new URL("http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL");
                 //http://www.bccs.uni.no/~pve002/wsdls/ebi-mafft.wsdl");
                 //http://www.webservicex.net/GenericNAICS.asmx?WSDL");
